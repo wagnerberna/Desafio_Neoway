23cdf2beaccda1715ad1a7cdbeb49da5bb036a9d
wagnerberna <cabelo29007@gmail.com>
2022-01-25 11:07:03 -0300

    data scraper page_start

--- api/data_scraper.py
+++ api/data_scraper.py
@@ -4,40 +4,32 @@ import requests
 
 
 class CandidateScrape:
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, start_page, stop_page=4671):
+        self.start_page = start_page
+        self.stop_page = stop_page
 
-    def scrape_url(self, start_page):
-        page = start_page
+    def scrape_url(self):
+        page = self.start_page
         cpfs_page = []
-        endpoint = f'{self.url}{page}'
+        endpoint = f"https://sample-university-site.herokuapp.com/approvals/{page}"
         print(endpoint)
         req = requests.get(endpoint)
-
         soup = BeautifulSoup(req.text, 'lxml')
 
-        print(soup.contents)
-
-        print("---FOR para pegar os Filhos----")
         for child in soup.body.contents:
             print(child.name)
             if child.name == 'li':
                 cpfs_page.append(child.get_text())
-                # print(child)
-                # print(child.a.get('href'))
-                # cpf = child.get_text()
             if child.name == 'div':
                 print('fim da página')
                 self.scrape_cpfs_page(cpfs_page)
 
         print(cpfs_page)
-        print(cpfs_page is array)
 
     def scrape_cpfs_page(self, cpfs_page):
-        pass
-        # if cpfs_page is array:
-        for cpf in cpfs_page:
-            self.scrape_candidate(cpf)
+        if type(cpfs_page) is list:
+            for cpf in cpfs_page:
+                self.scrape_candidate(cpf)
 
     def scrape_candidate(self, candidate_cpf):
         cpf = candidate_cpf
@@ -65,8 +57,7 @@ class CandidateScrape:
 
 if __name__ == "__main__":
 
-    url = "https://sample-university-site.herokuapp.com/approvals/"
-    scraper_candidates = CandidateScrape(url)
+    scraper_candidates = CandidateScrape(3)
 
     # scraper_candidates.scrape_candidate("178.422.117-11")
-    scraper_candidates.scrape_url(1)
+    scraper_candidates.scrape_url()
