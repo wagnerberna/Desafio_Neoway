838e6dbe310a15b3e609825b4cf8f67990b5a275
wagnerberna <cabelo29007@gmail.com>
2022-01-25 19:53:06 -0300

    data scraper completed

--- api/data_scraper.py
+++ api/data_scraper.py
@@ -1,6 +1,7 @@
 from array import array
 from bs4 import BeautifulSoup
 import requests
+import time
 
 
 class CandidateScrape:
@@ -9,27 +10,31 @@ class CandidateScrape:
     #     self.stop_page = stop_page
 
     def scrape_url(self, start_page, stop_page=4671):
-        page = start_page
+
+        for page in range(start_page, stop_page + 1):
+            print(f'page: {page}')
+            time.sleep(5)
+            self.scrape_page(page)
+
+    def scrape_page(self, page):
         cpfs_page = []
         endpoint = f"https://sample-university-site.herokuapp.com/approvals/{page}"
-        print(endpoint)
-        req = requests.get(endpoint)
+        req = requests.get(endpoint, timeout=2)
         soup = BeautifulSoup(req.text, 'lxml')
 
         for child in soup.body.contents:
-            print(child.name)
             if child.name == 'li':
                 cpfs_page.append(child.get_text())
             if child.name == 'div':
                 print('fim da página')
-                self.scrape_cpfs_page(cpfs_page)
-
+                self.scrape_cpfs_list(cpfs_page)
         print(cpfs_page)
 
-    def scrape_cpfs_page(self, cpfs_page):
+    def scrape_cpfs_list(self, cpfs_page):
         if type(cpfs_page) is list:
             for cpf in cpfs_page:
                 self.scrape_candidate(cpf)
+                print(cpf)
 
     def scrape_candidate(self, candidate_cpf):
         cpf = candidate_cpf
@@ -60,4 +65,4 @@ if __name__ == "__main__":
     scraper_candidates = CandidateScrape()
 
     # scraper_candidates.scrape_candidate("178.422.117-11")
-    scraper_candidates.scrape_url(5)
+    scraper_candidates.scrape_url(9, 10)
