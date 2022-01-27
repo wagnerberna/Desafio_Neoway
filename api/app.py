77bb039c3ea74a9b69bb4e618364e70196dbda27
wagnerberna <cabelo29007@gmail.com>
2022-01-26 21:49:19 -0300

    add messages

--- api/app.py
+++ api/app.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_restful import Api
-from controller.candidate import Candidates, Candidate, CandidateRegister
+from controller.candidate import CandidatesController, CandidateController, CandidateControllerRegister
 
-app = Flask(__name__)
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -14,9 +13,9 @@ def create_db():
     db.create_all()
 
 
-api.add_resource(Candidates, "/candidates")
-api.add_resource(Candidate, "/candidate/<string:candidate_id>")
-api.add_resource(CandidateRegister, "/register")
+api.add_resource(CandidatesController, "/candidates")
+api.add_resource(CandidateController, "/candidate/<string:candidate_id>")
+api.add_resource(CandidateControllerRegister, "/register")
 
 if __name__ == "__main__":
     from sql_alchemy import db
--- api/controller/candidate.py
+++ api/controller/candidate.py
@@ -1,13 +1,14 @@
 from flask_restful import Resource, reqparse
 from models.candidate import CandidateModel
+from service.messages import *
 
 
-class Candidates(Resource):
+class CandidatesController(Resource):
     def get(self):
         return {"candidates": [candidate.json() for candidate in CandidateModel.query.all()]}
 
 
-class CandidateRegister(Resource):
+class CandidateControllerRegister(Resource):
     attributes = reqparse.RequestParser()
     attributes.add_argument(
         "name",
@@ -27,65 +28,65 @@ class CandidateRegister(Resource):
     )
 
     def post(self):
-        # if CandidateModel.find_candidate(candidate_id):
-        #     return {"message": f"candidate ID: {candidate_id} alredy exists"}, 400
-        data = Candidate.attributes.parse_args()
+        data = CandidateController.attributes.parse_args()
         candidate_model = CandidateModel(**data)
+        # if CandidateModel.find_candidate(candidate_id):
+        # return {"message": f"candidate ID: {candidate_id} alredy exists"}, 400
         try:
             candidate_model.save_candidate()
         except:
-            return {"message": "An interna erro ocurred trying to save candidate."}, 500
+            return SAVE_INTERNAL_ERROR, 500
         return candidate_model.json()
 
 
-class Candidate(Resource):
+class CandidateController(Resource):
     attributes = reqparse.RequestParser()
     attributes.add_argument(
         "name",
         type=str,
         required=True,
-        help="The field 'name' cannot be left blank.",
+        help=FIELD_NOT_BLANK.format('name'),
     )
     attributes.add_argument(
         "score",
         type=float,
         required=True,
-        help="the field 'score' cannot bel left blank",
+        help=FIELD_NOT_BLANK.format('score'),
     )
     attributes.add_argument(
         "cpf",
         type=str,
         required=True,
-        help="The field 'CPF' cannot be left blank.",
+        help=FIELD_NOT_BLANK.format('cpf'),
     )
     attributes.add_argument(
         "valid_cpf",
         type=bool,
         required=True,
-        help="The field 'valid CPF' cannot be left blank.",
+        help=FIELD_NOT_BLANK.format('valid_cpf'),
     )
 
     def get(self, candidate_id):
         candidate_model = CandidateModel.find_candidate(candidate_id)
         if candidate_model:
             return candidate_model.json(), 200
-        return {"message": "candidate not found."}, 404
+        return NOT_FOUND, 404
 
     def put(self, candidate_id):
-        data = Candidate.attributes.parse_args()
+        data = CandidateController.attributes.parse_args()
         candidate_model = CandidateModel.find_candidate(candidate_id)
         try:
             candidate_model.update_candidate(**data)
             return candidate_model.json(), 200
         except:
-            return {"message": "An interna erro ocurred trying to save candidate."}, 500
+            return SAVE_INTERNAL_ERROR, 500
 
     def delete(self, candidate_id):
         candidate_model = CandidateModel.find_candidate(candidate_id)
         if candidate_model:
             try:
                 candidate_model.delete_candidate()
-                return {"message": "candidate deleted."}
+                return DELETED, 200
             except:
-                return {"message": "An interna erro ocurred trying to delete candidate."}, 500
-        return {"message": "candidate not found."}, 404
+                return DELETED_INTERNAL_ERROR, 500
+        return NOT_FOUND, 404
